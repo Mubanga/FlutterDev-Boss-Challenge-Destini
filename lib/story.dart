a9ca44479756f13bcd775c2a095e70a7ffb5780d
Mubanga <mubangeriseme@yahoo.com>
2019-08-05 05:47:23 +0200

    Destini: TODO's Completed: Step 5 - Create a new class called StoryBrain.
    Step 6 - import the story.dart file into this file.
    
    Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
    
    Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.

--- lib/story.dart
+++ lib/story.dart
@@ -1,7 +1,19 @@
 class Story {
-  String _storyTitle;
-  String _choice_1;
-  String _choice_2;
+  String storyTitle;
+  String choice1;
+  String choice2;
 
-  Story(this._storyTitle, this._choice_1, this._choice_2);
+  Story({this.storyTitle, this.choice1, this.choice2});
+
+//  String get storyTitle {
+//    return _storyTitle;
+//  }
+//
+//  String get choice1 {
+//    return _choice_1;
+//  }
+//
+//  String get choice2 {
+//    return _choice_2;
+//  }
 }
