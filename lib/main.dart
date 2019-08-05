b270a6ef1855a973202143b1ae850f64aa836e33
Mubanga <mubangeriseme@yahoo.com>
2019-08-05 05:57:37 +0200

    Destini: TODO's Completed: Step 13 - Use the storyBrain to get the text for choice 1. Step 14 - Use the storyBrain to get the text for choice 2.

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,6 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     storyModel.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
@@ -76,7 +75,6 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                     storyModel.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
